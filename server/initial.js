99b6f1cea5cd69726eb8088e6e5046485657b340
kvervel <joridjonland@gmail.com>
2017-01-21 17:14:37 +0000

    added jQuery and did some other stuff

--- server/initial.js
+++ server/initial.js
@@ -1,11 +1,16 @@
 Meteor.startup(function () {
-	if (Users.find().count() == 0) {
-		[{
+    if (Users.find().count() == 0) {
+        [{
+            username: "pussyLicker69",
+            about: "Here to fuck and suck, and he's all out of fuck",
+            objective: "get the bitches"
+        },
+		{
 			username: "John",
-      about: "Pretty gaaay.",
-      objective: "hack the planet"
+			about: "Pretty gaaay.",
+			objective: "hack the planet"
 		}].forEach(function (entry) {
-		Users.insert(entry);
-		});
-	}
+            Users.insert(entry);
+        });
+    }
 });
