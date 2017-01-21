a1752a5c6fd654b1ef2f72eeae6d63569bfa5621
Harley Wright <hwrig002@gold.ac.uk>
2017-01-21 20:39:23 +0000

    Lots of JS, please be more that CSS now <3

--- client/main.js
+++ client/main.js
@@ -15,12 +15,24 @@ Router.route("/", function () {
 	this.render("home");
 });
 
+Router.route("/startmenu", function() {
+	this.render("startmenu");
+});
+
+Router.route("/newgame", function() {
+	this.render("newgame");
+});
+
 Router.route("/info", function () {
 	this.render("info");
 });
 
 Router.route("/messageboard", function () {
 	this.render("messageboard");
+});
+
+Router.route("/joingame", function () {
+	this.render("joingame");
 });
 
 Router.route("/about", function () {
@@ -71,4 +83,48 @@ Template.info.helpers({
 
 Template.registerHelper("formatDate", function (date) {
   return moment(date).format("DD MMM HH:mm");
+});
+
+Template.startmenu.events({
+  'click #btn-new-game': function () {
+    Router.go("/newgame");
+  },
+  'click #btn-join-game': function () {
+    Router.go("/joingame");
+  },
+	'click #btn-start-back': function () {
+    Router.go("/");
+  }
+});
+
+Template.joingame.events({
+  'click #btn-join-back': function (e) {
+		e.preventDefault();
+    Router.go("/startmenu");
+  }
+});
+
+
+
+Template.createGame.events({
+  'submit #create-game': function (event) {
+    var playerName = event.target.playerName.value;
+    if (!playerName || Session.get('loading')) {
+      return false;
+    }
+    var game = generateNewGame();
+    var player = generateNewPlayer(game, playerName);
+    Meteor.subscribe('games', game.accessCode);
+    Session.set("loading", true);
+    Meteor.subscribe('players', game._id, function onReady(){
+      Session.set("gameID", game._id);
+      Session.set("playerID", player._id);
+      Session.set("currentView", "lobby");
+    })
+    return false;
+  },
+  'click #btn-cre-back': function (e) {
+		e.preventDefault();
+    Router.go("/startmenu");
+  }
 });
